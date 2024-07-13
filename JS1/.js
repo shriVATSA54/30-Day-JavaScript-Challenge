96c6e34a58e8d931a6fc079f6350a2a2dfa93766
shriVATSA 54 <shrivatsaddesai@gmail.com>
2024-07-13 21:32:10 +0530

    Day01

---
+++ JS1/.js
@@ -0,0 +1,3 @@
+ HEllojs
+ ,ns d,
+ ,sdm 
---
+++ JS1/Day01.js
@@ -0,0 +1,61 @@
+// Activity 1
+//Task1
+var a = 5;
+console.log(a);
+//Task2
+let b = "Hello World";
+console.log(b);
+
+// Activity 2
+//Task1
+const c = true;
+console.log(c);
+
+// Activity 3
+//Task1
+let d = [1, 2, 3, 4, 5];
+let Activity = {
+    Task_1: "Done",
+    Task_2: "Done",
+    TotalTask: 6
+
+}
+console.log(typeof Activity);
+console.log(Activity + typeof Activity.Task_1 + typeof Activity.Task_2);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+
+//Activity4
+//Task1
+let e = 3;
+e = 5;
+console.log("e " + e)
+
+
+//note we can declare twice using var and also initialize it
+var f = 3;
+var f = 6;
+console.log("f " + f)
+
+
+//Activity 5
+const g = 8;
+console.log("g " + g)
+
+try {
+    g = 15;
+} catch (error) {
+    console.log("error is  ", error)
+
+}
+
+console.log("g " + g)
+
+
+
+
+
+
